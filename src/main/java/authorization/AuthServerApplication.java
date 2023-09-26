25aea7bb72f9b2da13268660f9dee033373edcfe
Kidchai <o.naibova@gmail.com>
2023-09-26 13:09:30 -0500

    Add User, UserRepository, create default filter chain

--- src/main/java/authorization/AuthServerApplication.java
+++ src/main/java/authorization/AuthServerApplication.java
@@ -1,7 +1,10 @@
 package authorization;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AuthServerApplication {
@@ -10,4 +13,14 @@ public class AuthServerApplication {
         SpringApplication.run(AuthServerApplication.class, args);
     }
 
+    @Bean
+    public ApplicationRunner dataLoader(
+            UserRepository repo, PasswordEncoder encoder) {
+        return args -> {
+            repo.save(
+                    new User("admin1", encoder.encode("password"), "ROLE_ADMIN"));
+            repo.save(
+                    new User("admin2", encoder.encode("password"), "ROLE_ADMIN"));
+        };
+    }
 }
